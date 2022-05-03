fb1bd7ea0b4911ef581392506653014fdd2e5d9f
Michael-Deadrastick <104574183+Michael-Deadrastick@users.noreply.github.com>
2022-05-03 09:18:14 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,61 @@
-function setup() {
-  createCanvas(400, 400);
+kaboom({
+  scale: 3,
+  background: [10,150,200],
+})
+loadSpriteAtlas("https://kaboomjs.com/sprites/dungeon.png", "atlas.json");
+
+const levelConfig = {
+  width: 16,
+  height: 16,
+  pos: vec2(32,32),
+  "w": () => [
+    "wall",
+    sprite("wall"),
+    area(),
+    solid()
+  ],
+  "r":() => [
+    "enemy",
+    sprite("redGuy",{
+      "anim":"run"
+    }),
+    area({
+      "scale":0.50
+    }),
+    origin("center"),
+    {
+      "xVel":30
+    }
+  ],
+  "sword": {
+		"x": 322,
+		"y": 81,
+		"width": 12,
+		"height": 30
+	},
+  //from https://kaboomjs.com/play?demo=spriteatlas
 }
 
-function draw() {
-  background(220);
-}
+const levels = [
+  [
+    "   s    r   ",
+    " wwwwwwwwww"
+  ]
+]
+
+scene("game",() => {
+  
+  const level = addLevel(levels[0],levelConfig)
+
+  const player = add([
+      sprite("hero"),
+      pos(level.getPos(2,0)),
+      area({scale:0.5}),
+      solid(),
+      origin("bot"),
+      body()
+  ]);
+  player.play("idle")
+})
+
+go("game")
